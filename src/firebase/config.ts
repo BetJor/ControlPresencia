61070dc283744c0eb6dc9f968fe92378ec73195b
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-10-17 09:02:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3555195357-2f7ec",
+  "appId": "1:172357360231:web:7d4b46f85c0de33a74d2c2",
+  "apiKey": "AIzaSyDe8YbnscWlCDSSuhH0ZrZvsrm2KDrVNKM",
+  "authDomain": "studio-3555195357-2f7ec.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "172357360231"
+};
