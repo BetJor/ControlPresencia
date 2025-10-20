0617a5be15ab694c59b8e9e492e591f405d6fc9b
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-10-20 08:06:39 +0000

    Ara si que ha canviat, però la veig massa gran, fes-la de 40

--- src/app/(app)/visitors/page.tsx
+++ src/app/(app)/visitors/page.tsx
@@ -114,7 +114,7 @@ export default function VisitorsPage() {
             <div className="fixed bottom-6 right-6">
                 <FavoriteVisitorForm>
                     <Button size="icon" className="rounded-full h-14 w-14 shadow-lg">
-                        <Plus style={{ width: '44px', height: '44px' }} />
+                        <Plus style={{ width: '40px', height: '40px' }} />
                         <span className="sr-only">Añadir Visita Favorita</span>
                     </Button>
                 </FavoriteVisitorForm>
