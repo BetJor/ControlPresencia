fad9cd96b16919687044d46eb8d026bc50e86893
Elisabet Jordana Quer <ejordana@costaisa.com>
2025-10-20 08:02:56 +0000

    Vull que la icona sigui mes gran, de 30 per 30 (_for element <Button>_)

--- src/app/(app)/visitors/page.tsx
+++ src/app/(app)/visitors/page.tsx
@@ -114,7 +114,7 @@ export default function VisitorsPage() {
             <div className="fixed bottom-6 right-6">
                 <FavoriteVisitorForm>
                     <Button size="icon" className="rounded-full h-14 w-14 shadow-lg">
-                        <Plus className="h-6 w-6" />
+                        <Plus className="h-8 w-8" />
                         <span className="sr-only">Añadir Visita Favorita</span>
                     </Button>
                 </FavoriteVisitorForm>
